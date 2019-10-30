7f7d4362da706dba7e743f214d09630bd6889386
Tahseen Rasheed Chowdhury <35876394+TahseenSust@users.noreply.github.com>
2019-10-30 07:55:37 +0600

    Update bisection.m

--- bisection.m
+++ bisection.m
@@ -1,18 +1,17 @@
-function value=bisection(f,a,b);
+function bisection(f,a,b);
   pkg load symbolic;
-  syms x;
-  func=inline(f);
-d = 1e-10;
-while b-a >= d
-    m = (a + b)/2;
-    printf("%d %d %d %d %d %d\n",a,b,func(a),func(b),m,func(m));
-    
-    if func(m)*func(a) < 0
-        b = m;
-    else
-        a = m;
-  endif
+  sym x;
+  fun=inline(f);
+  d=1e-10;
   
-endwhile
-value=m;
+  while b-a>=d
+    m=(a+b)/2;
+    if fun(a)*fun(m)<0
+      b=m;
+    else
+      a=m;
+     endif
+   endwhile
+   m
+  
 endfunction
